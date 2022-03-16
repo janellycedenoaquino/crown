9bfd63ba383d598dca74d55892e6a90cbaca6ce9
janellycedenoaquino <janellycedenoo@gmail.com>
2022-03-16 02:32:39 -0400

    crud for products work. tested with insomnia checked with postico =)

---
+++ src/api/index.ts
@@ -0,0 +1,9 @@
+import { Router, Request, Response } from "express";
+import products from "./products";
+const router: Router = Router();
+export default router;
+
+
+// router.use("/users", users)
+router.use("/products", products);
+// router.use("/orders", orders)
---
+++ src/api/products.ts
@@ -0,0 +1,66 @@
+import Product from "../db/models/Product";
+import { Router, Request, Response, NextFunction } from "express";
+const router: Router = Router();
+
+export default router;
+
+router.get("/", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const products = await Product.findAll();
+    if (!products) {
+      res.status(400).send("that product does not exist");
+      return;
+    }
+    res.send(products);
+  } catch (error) {
+    res.send("there are no products available");
+    next(error);
+  }
+});
+
+router.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    // const products = req.params.id
+    const product = await Product.findByPk(req.params.id);
+    if (!product) {
+      res.status(400).send("that product does not exist");
+      return;
+    }
+    res.send(product);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    // const products = req.params.id
+    const product = await Product.findByPk(req.params.id);
+
+    res.send(await product?.update(req.body));
+  } catch (error) {
+    next(error);
+  }
+});
+router.delete(
+  "/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      // const products = req.params.id
+      const product = await Product.findByPk(req.params.id);
+      await product?.destroy();
+      res.send(`item was deleted`);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// if no other works
+router.use((req: Request, res: Response, next: NextFunction) => {
+  res
+    .status(404)
+    .send("You look a little lost there...this page does not exist");
+  const error = Error("DNE");
+  next(error);
+});
